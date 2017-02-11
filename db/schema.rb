0ed3bfff444440a745b201f28016c0b746c27f46
Vitali Makarov <405112@gmail.com>
2017-02-11 15:44:05 +0300

    rails_apps_composer: set up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211124326) do
+ActiveRecord::Schema.define(version: 20170211124335) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "role",       limit: 4
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    email "MyString"
+    name "Test User"
+    email "test@example.com"
+    password "please123"
+
+    trait :admin do
+      role 'admin'
+    end
+
   end
 end
